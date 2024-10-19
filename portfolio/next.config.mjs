2d554d740d5b36706763cb96429326a619d490dc
Diivvuu <divyanshubawa9@gmail.com>
2024-10-20 00:12:26 +0530

    lint remove

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // This will ignore ESLint checks during the build
+  },
+};
 
 export default nextConfig;
